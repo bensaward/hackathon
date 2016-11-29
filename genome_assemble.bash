39fe1f9c1547cd228edd089efa55175830651542
bensaward <benjamin.saward@gmail.com>
2016-11-29 12:57:30 +0000

    completed first stage of the assembly pipeline

--- genome_assemble.bash
+++ genome_assemble.bash
@@ -3,13 +3,13 @@
 ###################################################
 #           	 GLOBAL PATHS                     #
 ###################################################
-
-FILENAME=$1
-HOMEDIR=#puthomedirhere
+FILEMODE=$1
+FILENAME=$2
+CURRENTPATH=$(pwd)
 export PATH=$HOMEDIR/nanopolish:$PATH
-if [-z $1]
+if [-z $1 |-z $2] #if either command line argument 1 or argument 2 are empty
 	then
-		echo "genome_assemble.bash [fasta file]"
+		echo "genome_assemble.bash [fasta|gz] [fasta file|tar.gz folder]"
 fi
 	
 
@@ -17,5 +17,13 @@ fi
 ###################################################
 #             Assembly instructions		  #
 ###################################################
+if [FILEMODE == "fasta"]
+	then
+		tar -xvf $CURRENTPATH/$FILENAME
+		$FILENAME=${FILENAME%%.tar.gz} #cut the extension from FILENAME
+fi
 
+poretools fasta $CURRENTPATH/$FILENAME > temp.fasta
+
+########## DONE GETTING FASTA FILES ##############
 
