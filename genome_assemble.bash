592e7fff03ebc3d2f8ff735bf0382ca45020ee6a
bensaward <benjamin.saward@gmail.com>
2016-11-29 17:41:51 +0000

    added src folder

--- genome_assemble.bash
+++ genome_assemble.bash
@@ -6,7 +6,11 @@
 FILEMODE=$1
 FILENAME=$2
 CURRENTPATH=$(pwd)
-export PATH=$HOMEDIR/nanopolish:$PATH
+export PATH=$HOMEDIR/nanopolish:$PATH  #add nanopolish
+export PATH=$HOMEDIR/canu/Linux-amd64/bin/canu:$PATH #add canu
+export PATH=$HOMEDIR/
+
+
 if [-z $1 |-z $2] #if either command line argument 1 or argument 2 are empty
 	then
 		echo "genome_assemble.bash [fasta|gz] [fasta file|tar.gz folder]"
