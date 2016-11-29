804b95b8c70ad8bda7e0a7160ae2f9f9952da5a4
bensaward <benjamin.saward@gmail.com>
2016-11-29 23:04:44 +0000

    assembled the 26kb of genome, updated bash file to include commands used

--- genome_assemble.bash
+++ genome_assemble.bash
@@ -3,27 +3,30 @@
 ###################################################
 #           	 GLOBAL PATHS                     #
 ###################################################
-FILEMODE=$1
-FILENAME=$2
+#FILEMODE=$1
+FILENAME=$1
+HOMEDIR=/home/ubuntu
 CURRENTPATH=$(pwd)
 export PATH=$HOMEDIR/nanopolish:$PATH
+export PATH=$HOMEDIR/canu/Linux-amd64/bin/:$PATH
+export PATH=$HOMEDIR/transrate/:$PATH
+
+
 if [-z $1 |-z $2] #if either command line argument 1 or argument 2 are empty
 	then
 		echo "genome_assemble.bash [fasta|gz] [fasta file|tar.gz folder]"
 fi
 	
-
-
 ###################################################
 #             Assembly instructions		  #
 ###################################################
-if [FILEMODE == "fasta"]
-	then
-		tar -xvf $CURRENTPATH/$FILENAME
-		$FILENAME=${FILENAME%%.tar.gz} #cut the extension from FILENAME
-fi
 
 poretools fasta $CURRENTPATH/$FILENAME > temp.fasta
+FILENAME=${FILENAME%%.}
 
+############ Could use soley canu? ###############
 ########## DONE GETTING FASTA FILES ##############
 
+canu -p $FILENAME -d $FILENAME-nanopore genomeSize='13.8m' gnuplotTested=true -nanopore-raw temp.fasta
+### bodge gnuplotTested=true for server errors ###
+############ This should assemble ################
